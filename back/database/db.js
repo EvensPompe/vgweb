447425a1a38460dea9e488fb5e42d3094b1a53f8
Evens Pompe <evenspompe9593@gmail.com>
2019-09-26 00:06:48 +0200

    db sequelize v0.1

--- back/database/db.js
+++ back/database/db.js
@@ -1,8 +1,15 @@
-const sequelize = require('sequelize');
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+// const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const sequelize = new Sequelize("dbvgweb","root","",{
   host:'localhost',
-  user:"root",
-  database:"dbvgweb"
+  dialect:"mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 })
+
+module.exports = db;
