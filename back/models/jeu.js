1bbb17de43ddc5f5117a7377b5101da05aa4214d
Evens Pompe <evenspompe9593@gmail.com>
2019-09-26 01:04:32 +0200

    db sequelize models jeu v0.4

--- back/models/jeu.js
+++ back/models/jeu.js
@@ -16,28 +16,37 @@ module.exports = (sequelize, Sequelize) => {
         allowNull: false
       },
       nom_jeu: {
+        //On définit le type du champ en VARCHAR(255)
         type: Sequelize.DataTypes.STRING
       },
       jour_de_sortie: {
+        //On définit le type du champ en INT(11)
         type: Sequelize.DataTypes.INTEGER
       },
       mois_de_sortie: {
+        //On définit le type du champ en INT(11)
         type: Sequelize.DataTypes.INTEGER
       },
       annee_de_sortie: {
+        //On définit le type du champ en INT(11)
         type: Sequelize.DataTypes.INTEGER
       },
       synopsis: {
+        //On définit le type du champ en INT(11)
         type: Sequelize.DataTypes.STRING
       },
       prix: {
+        //On définit le type du champ en DECIMAL(7,2)
         type: Sequelize.DataTypes.DECIMAL(7, 2)
       },
       images: {
+        //On définit le type du champ en TEXT
         type: Sequelize.DataTypes.TEXT,
+        //On crée un getter pour parser le text en JSON
         get: function() {
           return JSON.parse(this.getDataValue("images"));
         },
+        //On crée un setter pour convertir le JSON en string
         set: function() {
           return this.setDataValue("images", JSON.stringify(value));
         }
@@ -52,7 +61,9 @@ module.exports = (sequelize, Sequelize) => {
         }
       }
     },{
+      //Ajoute automatiquement un champ created_at et un champ updated_at
       timestamps: true,
+      //met automatiquement un attribut 
       underscored: true
     })
 }
