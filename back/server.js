8c6cf9bca6ebf7c4d2d479f4a1d49ba007f6fef4
Evens Pompe <evenspompe9593@gmail.com>
2019-09-25 22:40:11 +0200

    serverjs v0.1

--- back/server.js
+++ back/server.js
@@ -1,3 +1,9 @@
+//SERVEUR NODEJS DE VGWEB
+
+// Déclaration de modules dans des variables
+
+//ExpressJS est un package permettant d'effectuer les routes, d'utiliser les moteurs de templates,
+//utiliser les requêtes HTTP, faire de l'API,...
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -7,3 +13,8 @@ let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+let port = process.env.PORT || 3000;
+app.listen(port,()=>{
+  console.log(`Serveur sur le port ${port}`);
+})
