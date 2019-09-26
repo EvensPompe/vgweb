ee0de17aae25baa53103d6e21a26bdc13d5d4694
Evens Pompe <evenspompe9593@gmail.com>
2019-09-27 00:10:02 +0200

    db sequelize import models

--- back/database/db.js
+++ back/database/db.js
@@ -10,7 +10,7 @@ On crée une nouvelle instance Sequelize, qui crée un connection avec la
 base de donnée mySQL "dbvgweb".
 On définit une taille de la pool de connection(minimum :0, maximum : 5),
 idle : temps qu'une connection peut être inative avant d'être enlever
-acquire : temps max que la pool essaye de lancer une connection avant de mettre une erreure ! 
+acquire : temps max que la pool essaye de lancer une connection avant de mettre une erreure !
 */
 const sequelize = new Sequelize("dbvgweb","root","",{
   host:'localhost',
@@ -35,6 +35,17 @@ sequelize
   });
 
   const db = {};
+
+//import des models à la base de donnée
+db.jeu = require('../models/jeu')(sequelize,Sequelize);
+db.genre = require('../models/genre')(sequelize,Sequelize);
+db.editDev = require('../models/editDev')(sequelize,Sequelize);
+db.note = require('../models/note')(sequelize,Sequelize);
+db.jeu_has_plateforme = require('../models/jeu_has_plateforme')(sequelize,Sequelize);
+db.plateforme = require('../models/plateforme')(sequelize,Sequelize);
+db.utilisateur = require('../models/utilisateur')(sequelize,Sequelize);
+db.plateforme_has_editDev = require('../models/plateforme_has_editDev')(sequelize,Sequelize);
+
 
   db.sequelize = sequelize;
   db.Sequelize = Sequelize;
