32b7764e2fa8c4fd74b236b7e36a7a226926ace6
Evens Pompe <evenspompe9593@gmail.com>
2019-09-27 23:12:58 +0200

    npm install axios

--- back/router/jeu.js
+++ back/router/jeu.js
@@ -1,6 +1,9 @@
 const express = require('express');
+/* On définit un router pour les opérations de type 'GET','POST',PUT ou 'DELETE'*/
 const router = express.Router();
 
 const db = require('../database/db');
+
+
 
 module.exports = router;
