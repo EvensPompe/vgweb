38a7d15ee34267f518f769841f842735d27fe777
Evens Pompe <evenspompe9593@gmail.com>
2019-10-01 22:15:21 +0200

    changement majeur babel webpack build + router utilisateur

--- back/router/utilisateur.js
+++ back/router/utilisateur.js
@@ -1,14 +1,29 @@
 const express = require('express');
+/*On définit le router pour les opérations de type GET POST PUT */
 const router = express.Router();
 
+//On uilise bcrypt pour crypter les mots de passe
+/*bcrypt est une fonction de hachage créée par Niels Provos et David Mazières.
+Elle est basée sur l'algorithme de chiffrement Blowfish et a été présentée lors de USENIX en 19991.
+En plus de l'utilisation d'un sel pour se protéger des attaques par table arc-en-ciel (rainbow table),
+bcrypt est une fonction adaptative, c'est-à-dire que l'on peut augmenter le nombre d'itérations pour la rendre plus lente.
+Ainsi elle continue à être résistante aux attaques par force brute malgré l'augmentation de la puissance de calcul.*/
 const bcrypt = require('bcrypt');
+//On utilise cors pour les autorisations http
 const cors = require('cors');
+//On utilise Jsonwebtoken pour les authentifications et
+//faire des tokens
 const jwt = require('jsonwebtoken');
+// On importe la base de donnée pour les tables
 const db = require('../database/db');
 
+//On génère avec le process une clé secrète
 process.env.SECRET_KEY = "secret";
 
+
+/*On inscrit un nouvel utilisateur*/
 router.post("/register",(req,res)=>{
+//On définit les informations reçus
   let userData ={
     nom: req.body.nom,
     email: req.body.email,
@@ -16,16 +31,25 @@ router.post("/register",(req,res)=>{
     presse: req.body.presse,
     admin: req.body.admin,
   }
+// On vérifie si l'utilisateur existe
   db.utilisateur.findOne({
+// en comparant les emails
     where:{email:req.body.email}
   }).then(user=>{
+//si l'utilisateur n'existe pas
     if (!user) {
+//On crypte le mot de passe en hachant avec 10 salt
       const hash = bcrypt.hashSync(userData.password,10)
+/*On remplace le mot de passe en clair par le mot de passe crypté*/
       userData.password = hash
+/*Avec les données, on va crée l'utilisateur*/
       db.utilisateur.create(userData).then(user=>{
+//On génère un token avec les données de l'utilisateur, la clé secrète et les options
         let token = jwt.sign(userData,process.env.SECRET_KEY,{
+//le token expire au bout de 12h
           expiresIn: "12h"
         })
+//On renvoie le token en objet JSON
         res.json({token:token})
       }).catch(err=>{
         res.json(err)
@@ -137,5 +161,25 @@ router.put("/modify/:email",(req,res)=>{
     res.json(err);
   })
 });
+
+router.delete("/delete/:id",(req,res)=>{
+  db.utilisateur.findOne({
+    where:{id:req.params.id}
+  }).then((user)=>{
+    if (user) {
+      user.destroy()
+      .then(()=>{
+        res.json('utilisateur supprimé')
+      }).catch(err=>{
+        res.json(err)
+      })
+    }else {
+      res.json("Cet utilisateur n'existe pas")
+    }
+
+  }).catch(err=>{
+    res.json(err)
+  })
+})
 
 module.exports = router;
